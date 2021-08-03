414bbe74edfd6a1548c58eb173a761b29793c2e6
Bora M. Alper <bora@boramalper.org>
2021-08-03 12:11:49 +0300

    minor refactoring and add license info
    Signed-off-by: Bora M. Alper <bora@boramalper.org>

--- mare.proto
+++ mare.proto
@@ -1,6 +1,4 @@
-// MIT License
-//
-// Copyright (c) 2021 Mert Bora Alper and EASE lab
+// Copyright (c) 2021 Mert Bora Alper and EASE Lab
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
