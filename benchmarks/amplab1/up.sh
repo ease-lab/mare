d1e5d9dfffbf64a74293888e4f2666661657a846
Bora M. Alper <bora@boramalper.org>
2021-08-06 14:41:29 +0300

    add amplab1 benchmark
    Signed-off-by: Bora M. Alper <bora@boramalper.org>

---
+++ benchmarks/amplab1/up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ -z "$AWS_ACCESS_KEY_ID" ] && echo 'Must set AWS_ACCESS_KEY_ID' && exit 1;
+[ -z "$AWS_SECRET_ACCESS_KEY" ] && echo 'Must set AWS_SECRET_ACCESS_KEY' && exit 1;
+
+kn service apply --wait-timeout 60 --filename worker.yml \
+  --env AWS_REGION=us-west-1 \
+  --env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+  --env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
